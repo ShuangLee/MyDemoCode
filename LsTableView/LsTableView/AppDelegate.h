bb86400c09d2d9d7a446ca356aa2a586fe564ac5
ShuangLee <shawn_acoder@sina.com>
2016-07-26 16:27:24 +0800

    first commit

---
+++ LsTableView/LsTableView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LsTableView
+//
+//  Created by 光头强 on 16/7/26.
+//  Copyright © 2016年 Ls. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
